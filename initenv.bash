1d5c64421949a11079cf65d308b40e8613ed1ecb
Yu Li <ylilarry@gmail.com>
2023-04-04 16:06:41 -0400

    Fix ci error

--- initenv.bash
+++ initenv.bash
@@ -1,3 +1,6 @@
 poetry env use python3.10
+poetry --version
+poetry run python --version
 poetry install
-poetry run nodeenv -n lts .nodevenv
+poetry run nodeenv --version
+poetry run nodeenv -n lts .nodevenv --verbose 
